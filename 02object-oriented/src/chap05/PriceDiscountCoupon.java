aeab4ff041f4e2032c4763cb59684d51f57aac94
liwengao <2806362136@qq.com>
2026-08-23 23:23:13 +0800

    1

---
+++ 02object-oriented/src/chap05/PriceDiscountCoupon.java
@@ -0,0 +1,15 @@
+package chap05;
+
+public class PriceDiscountCoupon implements Coupon  {
+    private int threshold;
+    private int discount;
+
+    public PriceDiscountCoupon(int threshold, int discount) {
+        this.threshold = threshold;
+        this.discount = discount;
+    }
+    @Override
+    public int calculateDiscount(double totalPrice) {
+        return totalPrice>=threshold?discount:0;
+    }
+}
